3a5b6c162ff50a4cdecc0e236c84059422175532
Junichiro Tobe <junichiro.tobe@gmail.com>
2009-07-25 10:19:38 +0900

    AutoLoader 部分に関して、1.7.x と1.8.x どちらにも対応するようにした

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -7,7 +7,7 @@
  */
 set_include_path(
                  '.' . PATH_SEPARATOR
-                 . '../library'. PATH_SEPARATOR
+                 . '../library/'. PATH_SEPARATOR
                  . '../application/default/models/'. PATH_SEPARATOR
                  . '../application/api/models/'    . PATH_SEPARATOR
                  . '../application/api/services/'  . PATH_SEPARATOR
@@ -15,11 +15,18 @@ set_include_path(
 );
 
 require_once 'Initializer.php';
+require_once('Zend/Version.php');
 
 // Set up autoload.
-require_once 'Zend/Loader/Autoloader.php';
-$autoloader = Zend_Loader_Autoloader::getInstance();
-$autoloader->setFallbackAutoloader(true);
+if(Zend_Version::compareVersion('1.8.0')) {
+    require_once 'Zend/Loader/Autoloader.php';
+    $autoloader = Zend_Loader_Autoloader::getInstance();
+    $autoloader->setFallbackAutoloader(true);
+}
+else {
+    require_once('Zend/Loader.php');
+    Zend_Loader::registerAutoload();
+}
 
 // Prepare the front controller.
 $frontController = Zend_Controller_Front::getInstance();
